90b14dc425003a1b7ff6b1b499e8385fba319e9c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-01-30 07:42:15 +0000

    Add python code

---
+++ inst/py/httppipe.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+
+from unixconn import UnixAdapter
+
+# Start with the unix socket version because that's fairly easy to get
+# going with and I can test it locally.  Then we can copy over all the
+# bits for the windows support and test that locally there.
+class Transporter(requests.Session):
+    def __init__(self, base_url):
+        super(Transporter, self).__init__()
+        self.base_url = base_url
+        self._custom_adapter = UnixAdapter(base_url)
+        self.mount('http://', self._custom_adapter)
+        self._unmount('https://')
+        self.base_url = 'http://localhost'
+
+    def _unmount(self, *args):
+        for proto in args:
+            self.adapters.pop(proto)
+
+    def simple_request(self, verb, url, headers, data = None):
+        res = self.request(method = verb, url = url, headers = headers,
+                           data = data)
+        headers = '\n'.join(
+            ['{}: {}'.format(*i) for i in res.raw.headers.items()])
+        content = res.content
+        is_binary = content.find('\x00') > 0
+        if is_binary:
+            content = [ord(i) for i in content]
+        return {'url': res.url,
+                'status_code': res.status_code,
+                'headers': headers,
+                'is_binary': is_binary,
+                'content': content}
